8df0a6d75cbebbb7019b7f15220808a60624d0e9
droobertzka <droobertzka@gmail.com>
2020-12-15 02:33:43 -0600

    day 4 part 2 (haskell)

--- day-4.hs
+++ day-4.hs
@@ -1,9 +1,43 @@
--- Part 1
+import qualified Data.Text as T
+import qualified Data.List as L
 
-parsePassport acc curr =
-    if null curr
-    then acc ++ [ curr ]
-    else (init acc) ++ [(last acc) ++ " " ++ curr]
+
+-- Validation Data (Shared)
+
+validateYear min max year =
+    let yearAsInt = read year :: Int
+    in
+        yearAsInt >= min && yearAsInt <= max
+
+validateBirthYr = validateYear 1920 2002
+validateIssueYr = validateYear 2010 2020
+validateExpirationYr = validateYear 2020 2030
+
+validateHeight str =
+    if L.isSuffixOf "in" str
+    then
+        let nums = takeWhile (/= 'i') str
+            height = read nums :: Int
+        in
+            height > 58 && height < 77
+    else if L.isSuffixOf "cm" str
+    then
+        let nums = takeWhile (/= 'c') str
+            height = read nums :: Int
+        in
+            height > 149 && height < 194
+    else
+        False
+
+
+validateHairColor (h:rest) =
+    h == '#' && length rest == 6 && all (`elem` "abcdef01234567890") rest
+
+validateEyeColor = (`elem` ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"])
+
+validatePassportId str =
+    length str == 9 && all (`elem` "0123456789") str
+
 
 requiredFields =
     [ "byr"
@@ -15,14 +49,54 @@ requiredFields =
     , "pid"
     ]
 
-validateField fields field =
-    filter (/= (takeWhile (/= ':') field)) fields
 
-validatePassport str =
+-- Part 1
+
+parsePassport acc curr =
+    if null curr
+    then acc ++ [ curr ]
+    else (init acc) ++ [(last acc) ++ " " ++ curr]
+
+validateField reqFields field =
+    filter (/= (takeWhile (/= ':') field)) reqFields
+
+hasRequiredFields str =
     (== 0) . length $ foldl validateField requiredFields (words str)
 
+parseInputs :: [String] -> [String]
+parseInputs = foldl parsePassport [""]
+
 partOne xs =
-    length $ filter validatePassport $ foldl parsePassport [""] xs
+    length $ filter hasRequiredFields $ parseInputs xs
+
+
+-- Part 2
+
+validateFieldValue field =
+    let [ keyAsText, valueAsText ] = T.splitOn (T.pack ":") (T.pack field)
+        key = T.unpack keyAsText
+        value = T.unpack valueAsText
+    in
+        -- TODO: why can't we use variables here?
+        -- (when we do, the compiler warns of redundancy
+        -- TODO: can we combine a let and a switch case here
+        -- and then assign a validate func and then apply to `value` once?
+        case key of
+            "byr" -> validateBirthYr value
+            "iyr" -> validateIssueYr value
+            "eyr" -> validateExpirationYr value
+            "hgt" -> validateHeight value
+            "hcl" -> validateHairColor value
+            "ecl" -> validateEyeColor value
+            "pid" -> validatePassportId value
+            "cid" -> True
+            _ -> False
+
+-- TODO: this could use some refactoring.
+partTwo xs =
+    let passportsWithReqFields = filter hasRequiredFields $ parseInputs xs
+    in
+        length $ filter (all validateFieldValue . words) passportsWithReqFields
 
 
 -- Input
