fc80ecd1d294f4c3e56a37fb107545962f0b1c4c
droobertzka <droobertzka@gmail.com>
2020-12-19 22:26:21 -0600

    day 6 part 2

--- src/DaySix.hs
+++ src/DaySix.hs
@@ -1,13 +1,23 @@
 module DaySix where
 
 import Data.List.Split (splitWhen)
-import Data.List (intercalate, nub)
+import Data.List (intercalate, intersect, nub)
 import FileIo (fileIo)
 
 
+-- Part One
 
 solvePartOne strings = sum $ map
     (length . nub . intercalate "")
     (splitWhen (== "") strings)
 
 partOne = fileIo "static/InputDaySix.txt" solvePartOne
+
+
+-- Part Two
+
+partTwo =
+    fileIo "static/InputDaySix.txt"
+        $ sum
+        . (map (length . foldl1 intersect))
+        . (splitWhen (== ""))
