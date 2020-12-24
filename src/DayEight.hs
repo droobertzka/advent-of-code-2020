0e44f7688e478ddbf609fc259df51a7c652059db
droobertzka <droobertzka@gmail.com>
2020-12-23 23:25:21 -0600

    day 8 part 2

--- src/DayEight.hs
+++ src/DayEight.hs
@@ -1,6 +1,7 @@
 module DayEight where
 
 import FileIo (fileIo)
+import Data.List (findIndices)
 
 
 -- Part One
@@ -22,9 +23,9 @@ replaceAt xs i =
         (op, num, isSecondTime) = head end
     in start ++ ((op, num, True) : tail end)
 
-operate :: [Operation] -> Int -> Int -> Int
+operate :: [Operation] -> Int -> Int -> (Int, Bool)
 operate operations i acc = if isSecondTime
-    then acc
+    then (acc, False)
     else case op of
         "nop" ->
             operate (replaceAt operations i) (i + 1) acc
@@ -34,9 +35,40 @@ operate operations i acc = if isSecondTime
             (acc + num)
         "jmp" ->
             operate (replaceAt operations i) (i + num) acc
+        "end" -> (acc, True)
     where (op, num, isSecondTime) = operations !! i
 
-solvePartOne raws = operate (map parse raws) 0 0
+solvePartOne raws = fst $ operate (map parse raws) 0 0
 
 partOne = fileIo "static/InputDayEight.txt" solvePartOne
 
+
+-- Part Two
+
+replaceElement i newEl xs = start ++ newEl : tail end
+    where (start, end) = splitAt i xs
+
+flipOp i ops =
+    let
+        (op, num, bool) = ops !! i
+        newOp = if op == "nop" then "jmp" else "nop"
+    in replaceElement i (newOp, num, bool) ops
+
+partTwoLoop :: [Operation] -> [Int] -> Int
+partTwoLoop ops (i : tailIndices) =
+    let
+        updatedOps         = flipOp i ops
+        (acc, didComplete) = operate updatedOps 0 0
+    in if didComplete
+        then acc
+        else partTwoLoop ops tailIndices
+
+solvePartTwo raws = partTwoLoop
+    parsedOps
+    (findIndices
+        (\(x, _, _) -> x == "nop" || x == "jmp")
+        parsedOps
+    )
+    where parsedOps = map parse raws ++ [("end", 0, False)]
+
+partTwo = fileIo "static/InputDayEight.txt" solvePartTwo
