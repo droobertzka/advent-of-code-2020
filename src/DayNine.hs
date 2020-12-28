9a930e550f3e913fd03cac7873dec14f2801e9c6
droobertzka <droobertzka@gmail.com>
2020-12-28 16:14:26 -0600

    day 9 part 2

--- src/DayNine.hs
+++ src/DayNine.hs
@@ -1,11 +1,13 @@
 module DayNine where
 
-import Data.List (find, findIndex)
-import Data.Maybe (isNothing)
+import Data.List (find)
+import Data.Maybe (isNothing, fromMaybe)
 import FileIo (fileIo)
 
 
 -- Part One
+
+parseNums = map (\x -> read x :: Int)
 
 doesNotHaveSum :: [Int] -> Int -> Bool
 doesNotHaveSum (n : ns) sum =
@@ -14,50 +16,62 @@ doesNotHaveSum (n : ns) sum =
            && doesNotHaveSum ns sum
            )
 
+partOneLoop :: [Int] -> Int -> [Int] -> (Int, Int)
 partOneLoop prevTwentyFiveNums i nums =
     let
         currentNum = nums !! i
         sumNotFound =
             doesNotHaveSum prevTwentyFiveNums currentNum
     in if sumNotFound
-        then currentNum
+        then (currentNum, i)
         else partOneLoop
             (tail prevTwentyFiveNums ++ [currentNum])
             (i + 1)
             nums
 
-solvePartOne prevLength unparsedNums = partOneLoop
-    (take prevLength parsedNums)
-    prevLength
-    parsedNums
-  where
-    parsedNums = map (\x -> read x :: Int) unparsedNums
+solvePartOne prevLength nums =
+    partOneLoop (take prevLength nums) prevLength nums
 
 partOne =
-    fileIo "static/InputDayNine.txt" $ solvePartOne 25
+    fileIo "static/InputDayNine.txt"
+        $ fst
+        . solvePartOne 25
+        . parseNums
 
 
--- Example Data
+-- Part Two
 
-examples =
-    [ "35"
-    , "20"
-    , "15"
-    , "25"
-    , "47"
-    , "40"
-    , "62"
-    , "55"
-    , "65"
-    , "95"
-    , "102"
-    , "117"
-    , "150"
-    , "182"
-    , "127"
-    , "219"
-    , "299"
-    , "277"
-    , "309"
-    , "576"
-    ]
+getContiguousNums :: [Int] -> Int -> Int -> Maybe [Int]
+getContiguousNums searchNums p1Answer howMany =
+    let
+        nums          = take howMany searchNums
+        isBelowLength = length nums < howMany
+        total         = sum nums
+        foundSum      = total == p1Answer
+    in if isBelowLength
+        then Nothing
+        else if foundSum
+            then Just nums
+            else getContiguousNums
+                (tail searchNums)
+                p1Answer
+                howMany
+
+partTwoLoop searchNums p1Anwser howMany = fromMaybe
+    (partTwoLoop searchNums p1Anwser (howMany + 1))
+    continguousNums
+  where
+    continguousNums =
+        getContiguousNums searchNums p1Anwser howMany
+
+solvePartTwo nums =
+    let
+        (p1Answer, i)   = solvePartOne 25 nums
+        searchNums      = take (i - 1) nums
+        continguousNums = partTwoLoop searchNums p1Answer 2
+    in minimum continguousNums + maximum continguousNums
+
+partTwo =
+    fileIo "static/InputDayNine.txt"
+        $ solvePartTwo
+        . parseNums
