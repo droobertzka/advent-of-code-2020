7ae80b79e339c19872f13f2a6c745e07c6b3980b
droobertzka <droobertzka@gmail.com>
2020-12-31 01:48:39 -0600

    day 12 part 2

--- src/DayTwelve.hs
+++ src/DayTwelve.hs
@@ -46,3 +46,58 @@ solvePartOne raws = abs x + abs y
     ((x, y), _) = foldl applyInstruction ((0, 0), 0) raws
 
 partOne = fileIo "static/InputDayTwelve.txt" solvePartOne
+
+
+-- Part Two
+
+data Position = Position
+    { shipCoords     :: Coords
+    , waypointCoords :: Coords
+    }
+
+moveToWaypoint :: Position -> Position
+moveToWaypoint (Position shipCoords wpCoords) = Position
+    (newX, newY)
+    wpCoords
+  where
+    [newX, newY] = uncurry (+) `map` [shipCoords, wpCoords]
+
+turnWaypoint :: Int -> Coords -> Coords
+turnWaypoint degrees (wpx, wpy) = case degrees of
+    90  -> (-wpy, wpx)
+    180 -> (-wpx, -wpy)
+    270 -> (wpy, -wpx)
+
+turn2 :: Position -> Char -> Int -> Position
+turn2 (Position shipCoords wpCoords) action degrees =
+    Position
+        shipCoords
+        (turnWaypoint translatedDeg wpCoords)
+  where
+    translatedDeg =
+        if action == 'L' then degrees else 360 - degrees
+
+applyInstruction2 :: Position -> String -> Position
+applyInstruction2 (Position (x, y) (wpx, wpy)) rawInstruction
+    = case action of
+        'N' -> Position (x, y) (wpx, wpy + n)
+        'E' -> Position (x, y) (wpx + n, wpy)
+        'W' -> Position (x, y) (wpx - n, wpy)
+        'S' -> Position (x, y) (wpx, wpy - n)
+        'F' ->
+            iterate
+                    moveToWaypoint
+                    (Position (x, y) (wpx, wpy))
+                !! n
+        _ -> turn2 (Position (x, y) (wpx, wpy)) action n
+    where (action, n) = parse rawInstruction
+
+solvePartTwo :: [String] -> Int
+solvePartTwo raws = abs x + abs y
+  where
+    (Position (x, y) _) = foldl
+        applyInstruction2
+        (Position (0, 0) (10, 1))
+        raws
+
+partTwo = fileIo "static/InputDayTwelve.txt" solvePartTwo
