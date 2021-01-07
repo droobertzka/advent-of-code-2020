70f86dab88c210f592b211ca546a5b2cc9665ccd
droobertzka <droobertzka@gmail.com>
2021-01-06 20:45:40 -0600

    day 13 part 2 (using Chinese remainder theorem)

--- src/DayThirteen.hs
+++ src/DayThirteen.hs
@@ -65,3 +65,41 @@ partTwo =
     fileIo
             "/Users/droobertzka/Dev/advent-of-code-2020/static/InputDayThirteen.txt"
         $ solvePartTwo 100000000000000
+
+
+-- Part Two (using Chinese Remainder Theorem)
+
+findX n modNum i = if i * n `mod` modNum == 1
+    then i
+    else findX n modNum (i + 1)
+
+makeRnX :: Int -> (Int, Int) -> Int
+makeRnX prod (c, modNum) =
+    let
+        r = modNum - c
+        prodOtherMods = prod `div` modNum
+        x = findX (prodOtherMods `mod` modNum) modNum 1
+    in r * prodOtherMods * x
+
+crt :: Int -> [(Int, Int)] -> Int
+crt prod pairs = total `mod` prod
+    where total = sum $ map (makeRnX prod) pairs
+
+makePairs
+    :: (Int, [(Int, Int)]) -> String -> (Int, [(Int, Int)])
+makePairs (i, pairs) str = if str == "x"
+    then (i', pairs)
+    else (i', pairs ++ [(i', parse str)])
+    where i' = i + 1
+
+solvePartTwo' :: [String] -> Int
+solvePartTwo' raws = crt (product $ map snd pairs) pairs
+  where
+    pairs = snd $ foldl
+        makePairs
+        (-1, [])
+        (splitOn "," $ last raws)
+
+partTwo' = fileIo
+    "/Users/droobertzka/Dev/advent-of-code-2020/static/InputDayThirteen.txt"
+    solvePartTwo'
